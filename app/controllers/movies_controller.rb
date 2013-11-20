44efebebe5887fd255e105db0ec5cdf57228415d
MatanPaul <matan@gumroad.com>
2013-11-19 20:47:03 -0800

    DB update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,35 @@ class MoviesController < ApplicationController
 	end
 
 	def new
+		@movie = Movie.new
 	end
-	
+
+	def create
+		@movie = Movie.new(safe_movie_params)
+		logger.info(@movie.inspect)
+		@movie.save
+		redirect_to root_path
+	end
+
+	def show
+		@movie = Movie.find(params[:id])
+	end
+
+	def edit
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+		@movie.update(safe_movie_params)
+		@movie.save
+		redirect_to root_path
+	end
+
+	private
+
+	def safe_movie_params
+		return params.require(:movie).permit(:title, :description, :year_released)
+	end
+
 end
