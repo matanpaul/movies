4ab708f7c4c7fe25825923e079a2e0b0129587f0
MatanPaul <matan@gumroad.com>
2013-11-21 16:47:29 -0800

    Movie app

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ActiveRecord::Base
 	default_scope order('year_released DESC')
 
 	def self.search_for(query)
-		Movie.where("title LIKE :query OR description LIKE :query OR year_released LIKE :query" , :query => "%#{query}%")
+		Movie.where("title LIKE :query OR description LIKE :query OR year_released = :query_plain" , :query => "%#{query}%", :query_plain => "#{query}")
 	end
 end
